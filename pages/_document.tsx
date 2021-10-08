4ad946a4458509caf76342789ccaf46987acc9ef
DanilovG103 <danilov2706@gmail.com>
2021-10-08 17:13:23 +0400

    feat: fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -34,7 +34,7 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;1,400&display=swap" rel="stylesheet"/>
           <link href="https://fonts.googleapis.com/css2?family=MuseoModerno:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
         </Head>
         <body>
