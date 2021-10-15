c612a93904b2cf1b9196392b3f5a8b792eb4ddaa
DanilovG103 <danilov2706@gmail.com>
2021-10-15 18:54:45 +0400

    fix: icon colors

--- src/components/icons/Courses.tsx
+++ src/components/icons/Courses.tsx
@@ -1,21 +1,26 @@
 import React from 'react'
-import { useColor } from 'src/hooks/useColor'
-import { useWindowSize } from 'src/hooks/useWindowSize'
+import styled from 'styled-components'
+import { colors } from 'src/theme/colors'
 
+const Wrapper = styled.div<{ isActive: boolean }>`
+  color: ${({ isActive }) => (isActive ? colors.white : colors.lightPurple)};
+  @media (max-width: 768px) {
+    color: ${({ isActive }) => (isActive ? colors.purple[1] : colors.gray[1])};
+  }
+`
 interface Props {
   isActive: boolean
 }
 
 export const CoursesIcon = ({ isActive }: Props) => {
-  const { width } = useWindowSize()
-  const { active, inactive } = useColor(width)
-
   return (
-    <svg width={17} height={21} fill="none">
-      <path
-        d="M13.572.917a2.639 2.639 0 012.637 2.635v13.896a2.635 2.635 0 01-2.637 2.635H3.502a2.639 2.639 0 01-2.637-2.636V3.552c0-1.207.811-2.223 1.918-2.537v1.579c-.291.218-.48.565-.48.958v13.894c0 .661.538 1.198 1.2 1.198h10.069c.662 0 1.199-.537 1.199-1.198V3.552c0-.661-.537-1.198-1.2-1.198h-2.157V.917h2.158zm-3.117 0v7.8c0 .715-.767.984-1.237.732l-.079-.05-2.038-1.232-1.993 1.2c-.48.344-1.276.133-1.36-.535l-.006-.114V.917h6.713zM9.017 2.354H5.18v5.293l1.508-.91a.886.886 0 01.785-.022l1.544.933V2.354h-.001z"
-        fill={isActive ? active : inactive}
-      />
-    </svg>
+    <Wrapper isActive={isActive}>
+      <svg width={17} height={21} fill="none">
+        <path
+          d="M13.572.917a2.639 2.639 0 012.637 2.635v13.896a2.635 2.635 0 01-2.637 2.635H3.502a2.639 2.639 0 01-2.637-2.636V3.552c0-1.207.811-2.223 1.918-2.537v1.579c-.291.218-.48.565-.48.958v13.894c0 .661.538 1.198 1.2 1.198h10.069c.662 0 1.199-.537 1.199-1.198V3.552c0-.661-.537-1.198-1.2-1.198h-2.157V.917h2.158zm-3.117 0v7.8c0 .715-.767.984-1.237.732l-.079-.05-2.038-1.232-1.993 1.2c-.48.344-1.276.133-1.36-.535l-.006-.114V.917h6.713zM9.017 2.354H5.18v5.293l1.508-.91a.886.886 0 01.785-.022l1.544.933V2.354h-.001z"
+          fill="currentColor"
+        />
+      </svg>
+    </Wrapper>
   )
 }
--- src/components/icons/Dashboard.tsx
+++ src/components/icons/Dashboard.tsx
@@ -1,65 +1,71 @@
 import React from 'react'
-import { useColor } from 'src/hooks/useColor'
-import { useWindowSize } from 'src/hooks/useWindowSize'
+import styled from 'styled-components'
+import { colors } from 'src/theme/colors'
+
+const Wrapper = styled.div<{ isActive: boolean }>`
+  color: ${({ isActive }) => (isActive ? colors.white : colors.lightPurple)};
+  @media (max-width: 768px) {
+    color: ${({ isActive }) => (isActive ? colors.purple[1] : colors.gray[1])};
+  }
+`
 
 interface Props {
   isActive: boolean
 }
 
 export const DashboardIcon = ({ isActive }: Props) => {
-  const { width } = useWindowSize()
-  const { active, inactive } = useColor(width)
-
   return (
-    <svg width={16} height={15} fill="none">
-      <mask id="prefix__a" fill={isActive ? active : inactive}>
-        <rect x={8.443} width={6.567} height={6.563} rx={1} />
-      </mask>
-      <rect
-        x={8.443}
-        width={6.567}
-        height={6.563}
-        rx={1}
-        stroke={isActive ? active : inactive}
-        strokeWidth={3}
-        mask="url(#prefix__a)"
-      />
-      <mask id="prefix__b" fill={isActive ? active : inactive}>
-        <rect x={8.443} y={7.5} width={6.567} height={6.563} rx={1} />
-      </mask>
-      <rect
-        x={8.443}
-        y={7.5}
-        width={6.567}
-        height={6.563}
-        rx={1}
-        stroke={isActive ? active : inactive}
-        strokeWidth={3}
-        mask="url(#prefix__b)"
-      />
-      <mask id="prefix__c" fill={isActive ? active : inactive}>
-        <rect y={7.5} width={6.567} height={6.563} rx={1} />
-      </mask>
-      <rect
-        y={7.5}
-        width={6.567}
-        height={6.563}
-        rx={1}
-        stroke={isActive ? active : inactive}
-        strokeWidth={3}
-        mask="url(#prefix__c)"
-      />
-      <mask id="prefix__d" fill={isActive ? active : inactive}>
-        <rect width={6.567} height={6.563} rx={1} />
-      </mask>
-      <rect
-        width={6.567}
-        height={6.563}
-        rx={1}
-        stroke={isActive ? active : inactive}
-        strokeWidth={3}
-        mask="url(#prefix__d)"
-      />
-    </svg>
+    <Wrapper isActive={isActive}>
+      <svg width={16} height={15} fill="none">
+        <mask id="prefix__a" fill="currentColor">
+          <rect x={8.443} width={6.567} height={6.563} rx={1} />
+        </mask>
+        <rect
+          x={8.443}
+          width={6.567}
+          height={6.563}
+          rx={1}
+          stroke="currentColor"
+          strokeWidth={3}
+          mask="url(#prefix__a)"
+        />
+        <mask id="prefix__b" fill="currentColor">
+          <rect x={8.443} y={7.5} width={6.567} height={6.563} rx={1} />
+        </mask>
+        <rect
+          x={8.443}
+          y={7.5}
+          width={6.567}
+          height={6.563}
+          rx={1}
+          stroke="currentColor"
+          strokeWidth={3}
+          mask="url(#prefix__b)"
+        />
+        <mask id="prefix__c" fill="currentColor">
+          <rect y={7.5} width={6.567} height={6.563} rx={1} />
+        </mask>
+        <rect
+          y={7.5}
+          width={6.567}
+          height={6.563}
+          rx={1}
+          stroke="currentColor"
+          strokeWidth={3}
+          mask="url(#prefix__c)"
+        />
+        <mask id="prefix__d" fill="currentColor">
+          <rect width={6.567} height={6.563} rx={1} />
+        </mask>
+        <rect
+          width={6.567}
+          height={6.563}
+          rx={1}
+          stroke="currentColor"
+          strokeWidth={3}
+          mask="url(#prefix__d)"
+        />
+      </svg>
+    </Wrapper>
   )
 }
